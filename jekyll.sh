175045173ff128d42a95c77895a8ce6d72d5df07
beyondskyml <tyust.yun@gmail.com>
2024-04-05 10:35:00 +0800

    update config

--- jekyll.sh
+++ jekyll.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #jekyll serve --watch # -d /Volumes/RAM/jekyll
-jekyll serve --watch --drafts --host=0.0.0.0 # -d /Volumes/RAM/jekyll
+jekyll serve --watch --drafts --host=127.0.0.1 # -d /Volumes/RAM/jekyll
 
 # 其他命令
 ## 新建 post
